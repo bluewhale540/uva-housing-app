29a6af70ea6671869178be871738530de7119de1
bluewhale540 <mathewcpham@gmail.com>
2021-10-29 16:05:51 -0400

    rating field

--- listings/forms.py
+++ listings/forms.py
@@ -5,19 +5,24 @@ from django import forms
 
 from listings.models import Listing, Review
 
+RATINGS = [tuple([x, x]) for x in range(1, 6)]
+
 
 class ReviewForm(forms.Form):
+    rating = forms.CharField(widget=forms.Select(choices=RATINGS))
     review = forms.CharField(widget=forms.Textarea)
 
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
     helper.layout = Layout(
+        Field('rating', rows="1"),
         Field('review', rows="5", css_class='input-xlarge'),
         FormActions(
             Submit('submit', 'Submit', css_class="btn-primary"),
             Submit('cancel', 'Cancel', css_class="btn-secondary", formnovalidate='formnovalidate'),
         )
     )
+
 
 '''
 class ReviewForm(forms.ModelForm):
